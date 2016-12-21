f63d12b7a0a6b8610b026d486d91134601366db0
Steve Weil <steve.weil@smartsheet.com>
2016-12-21 15:28:32 -0800

    Avoid build errors by ignoring snippets

--- config.rb
+++ config.rb
@@ -32,6 +32,7 @@ set :relative_links, true
 
 # Build Configuration
 configure :build do
+  ignore 'snippets/*'
   # If you're having trouble with Middleman hanging, commenting
   # out the following two lines has been known to help
   activate :minify_css
