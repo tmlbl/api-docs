66b7d9b80472ecb3731c8592b1382404cddf2379
Steve Weil <steve.weil@smartsheet.com>
2017-07-27 16:05:33 -0700

    Catch up to slate/master v2

--- lib/unique_head.rb
+++ lib/unique_head.rb
@@ -1,14 +1,16 @@
 # Unique header generation
 require 'middleman-core/renderers/redcarpet'
-require 'nokogiri'
 class UniqueHeadCounter < Middleman::Renderers::MiddlemanRedcarpetHTML
-  @@head_count = {}
+  def initialize
+    super
+    @head_count = {}
+  end
   def header(text, header_level)
-    friendly_text = Nokogiri::HTML(text).text.parameterize
-    @@head_count[friendly_text] ||= 0
-    @@head_count[friendly_text] += 1
-    if @@head_count[friendly_text] > 1
-      friendly_text += "-#{@@head_count[friendly_text]}"
+    friendly_text = text.parameterize
+    @head_count[friendly_text] ||= 0
+    @head_count[friendly_text] += 1
+    if @head_count[friendly_text] > 1
+      friendly_text += "-#{@head_count[friendly_text]}"
     end
     return "<h#{header_level} id='#{friendly_text}'>#{text}</h#{header_level}>"
   end
