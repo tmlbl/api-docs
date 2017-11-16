8aac6a0b7124baaf83755a185e1793917873c6a5
Steve Weil <steve.weil@smartsheet.com>
2017-11-16 11:28:40 -0800

    Look for dup header names globally
    As per https://github.com/lord/slate/issues/793

--- lib/unique_head.rb
+++ lib/unique_head.rb
@@ -1,16 +1,16 @@
 # Unique header generation
 require 'middleman-core/renderers/redcarpet'
+$head_count = {}
 class UniqueHeadCounter < Middleman::Renderers::MiddlemanRedcarpetHTML
   def initialize
     super
-    @head_count = {}
   end
   def header(text, header_level)
-    friendly_text = text.gsub(/<[^<]+>/,"").parameterize   # Strip HTML tags
-    @head_count[friendly_text] ||= 0
-    @head_count[friendly_text] += 1
-    if @head_count[friendly_text] > 1
-      friendly_text += "-#{@head_count[friendly_text]}"
+    friendly_text = text.gsub(/<[^<]+>/,"").parameterize
+    $head_count[friendly_text] ||= 0
+    $head_count[friendly_text] += 1
+    if $head_count[friendly_text] > 1
+      friendly_text += "-#{$head_count[friendly_text]}"
     end
     return "<h#{header_level} id='#{friendly_text}'>#{text}</h#{header_level}>"
   end
